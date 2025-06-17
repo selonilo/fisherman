5cb5e317cf896031b7335d8aff6671b31d2c49be
salih.cetin <salih.cetin@forte.com.tr>
2025-06-17 10:44:47 +0300

    webconfig fix

--- src/main/java/com/sc/fisherman/configuration/WebConfig.java
+++ src/main/java/com/sc/fisherman/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package com.sc.fisherman.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/fisherman/uploads/**")
-                .addResourceLocations("file:///C:/fisherman/uploads/");
+                .addResourceLocations("file:" + uploadPath + "/");
     }
 }
